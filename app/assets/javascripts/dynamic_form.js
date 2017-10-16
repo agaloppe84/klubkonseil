c0de42afad3a72f1fb1b8970c4c345c90a91dd15
aurelien galoppe <agaloppe@me.com>
2017-10-16 21:50:35 +0200

    done

--- app/assets/javascripts/dynamic_form.js
+++ app/assets/javascripts/dynamic_form.js
@@ -1,14 +1,14 @@
-$('#tools_question_3').change(function () {
-  var optionSelected = $(this).find("option:selected");
-  var valueSelected  = optionSelected.val();
-  var textSelected   = optionSelected.text();
-  if (valueSelected == "prestation de service") {
-    $('.middle').text("30 000");
+$('#custom-checkbox-q3 input').change(function () {
+  var optionSelected = $(this).val();
+
+  console.log(optionSelected);
+  if (optionSelected == "Prestation de service") {
+    $('#dyn-middle').text("30 000");
   }
-  if (valueSelected == "Vente de marchandises") {
-    $('.middle').text("80 000");
+  if (optionSelected == "Vente de marchandises") {
+    $('#dyn-middle').text("80 000");
   }
-  if (valueSelected == "Mixte") {
-    $('.middle').text("80 000");
+  if (optionSelected == "Mixte") {
+    $('#dyn-middle').text("80 000");
   }
  });
