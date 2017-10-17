87d4dbac3e8ef93cedc041ce333752051d7806a9
aurelien galoppe <agaloppe@me.com>
2017-10-17 10:56:51 +0200

    bugs

--- app/assets/javascripts/dynamic_form.js
+++ app/assets/javascripts/dynamic_form.js
@@ -1,12 +1,12 @@
 $('#custom-checkbox-q3 input').change(function () {
   var optionSelected = $(this).val();
   if (optionSelected == "Prestation de service") {
-    $('#dyn-middle').text("30 000");
+    $('#dyn-middle').text("30 000 €");
   }
   if (optionSelected == "Vente de marchandises") {
-    $('#dyn-middle').text("80 000");
+    $('#dyn-middle').text("80 000 €");
   }
   if (optionSelected == "Mixte") {
-    $('#dyn-middle').text("80 000");
+    $('#dyn-middle').text("80 000 €");
   }
  });
